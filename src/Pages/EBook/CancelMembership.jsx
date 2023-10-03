ac231a4814c8ff3a0a6e27e143cac8c6cbc9540c
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2023-10-03 11:52:18 -0300

    add validation to survey in ebook

--- src/Pages/EBook/CancelMembership.jsx
+++ src/Pages/EBook/CancelMembership.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const RadioItem = ({ label }) => (
+const RadioItem = ({ label, selected, onChange }) => (
   <div className="flex items-center my-3">
     <input
       className="form-check-input"
       type="radio"
       name="flexRadioDefault"
-      id={label.trim()}
+      id={label.replaceAll(' ', '')}
+      checked={selected == label.replaceAll(' ', '')}
+      onChange={() => onChange(label.replaceAll(' ', ''))}
     />
-    <label className="text-sm mx-1 text-gray-800" htmlFor={label.trim()}>
+    <label
+      className="text-sm mx-1 text-gray-800"
+      htmlFor={label.replaceAll(' ', '')}
+    >
       {label}
     </label>
   </div>
@@ -17,25 +23,55 @@ const RadioItem = ({ label }) => (
 export const CancelMembership = ({}) => {
   const navigate = useNavigate();
 
+  const [option, setOption] = useState('');
+  const [error, setError] = useState(false);
+
   const onSubmit = (e) => {
     e.preventDefault();
-    navigate('/ebook/books_offer');
+    if (option !== '') {
+      navigate('/ebook/books_offer');
+    } else {
+      setError(true);
+    }
   };
 
   return (
     <div className="membership survey">
       <h1 className="text-3xl my-8">Antes de que te vayas...</h1>
       <form onSubmit={onSubmit}>
-        <legend>
+        <legend className="text-xl my-4">
           ¿Cuál de los siguientes motivos te llevó a cancelar tu membresía?
         </legend>
-        <RadioItem label={'Tengo problemas técnicos'} />
-        <RadioItem label={'No uso ebookworld lo suficiente'} />
-        <RadioItem label={'Esta suscripción es demasiado cara'} />
+        {error && (
+          <p className="text-red-500 font-medium my-2">
+            Por favor, eliga una opcion
+          </p>
+        )}
         <RadioItem
+          selected={option}
+          onChange={setOption}
+          label={'Tengo problemas técnicos'}
+        />
+        <RadioItem
+          selected={option}
+          onChange={setOption}
+          label={'No uso ebookworld lo suficiente'}
+        />
+        <RadioItem
+          selected={option}
+          onChange={setOption}
+          label={'Esta suscripción es demasiado cara'}
+        />
+        <RadioItem
+          selected={option}
+          onChange={setOption}
           label={'No puedo encontrar el contenido específico que quiero leer'}
         />
-        <RadioItem label={'Otros problemas'} />
+        <RadioItem
+          selected={option}
+          onChange={setOption}
+          label={'Otros problemas'}
+        />
 
         <div className="my-1 relative">
           <textarea
@@ -44,7 +80,7 @@ export const CancelMembership = ({}) => {
           ></textarea>
           <label
             className="absolute top-0 left-0 p-2 text-sm text-gray-600"
-            for="floatingTextarea2"
+            htmlFor="floatingTextarea2"
           >
             Por favor explicanos los motivos
           </label>
