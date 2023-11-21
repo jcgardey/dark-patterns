3b4eb7bbbd59c9a2d4e6e0358149a3a051213932
juliangrigera <juliangrigera@gmail.com>
2023-11-21 12:33:53 +0000

    Roomio: fix search Ebook: add translations

--- src/Pages/EBook/Home.jsx
+++ src/Pages/EBook/Home.jsx
@@ -17,28 +17,28 @@ export const Home = () => {
           <ul>
             <li>
               <Link to="/ebook">
-                <i className="fa-solid fa-sharp fa-house"></i> Inicio
+                <i className="fa-solid fa-sharp fa-house"></i>  {t('Ebook.Home')}
               </Link>
             </li>
             <li>
               <a href="javascript:void(0)">
-                <i className="fa-solid fa-sharp fa-bookmark"></i> Guardados
+                <i className="fa-solid fa-sharp fa-bookmark"></i> {t('Ebook.Saved')}
               </a>
             </li>
             <li className="separator"></li>
             <li>
               <a href="javascript:void(0)">
-                <i className="fa-solid fa-sharp fa-star"></i> Recomendados
+                <i className="fa-solid fa-sharp fa-star"></i> {t('Ebook.Recommended')}
               </a>
             </li>
             <li>
               <a href="javascript:void(0)">
-                <i className="fa-solid fa-sharp fa-book"></i> Libros
+                <i className="fa-solid fa-sharp fa-book"></i> {t('Ebook.Books')}
               </a>
             </li>
             <li>
               <a href="javascript:void(0)">
-                <i className="fa-solid fa-sharp fa-headphones"></i> Audiolibros
+                <i className="fa-solid fa-sharp fa-headphones"></i> {t('Ebook.Audiobooks')}
               </a>
             </li>
           </ul>
