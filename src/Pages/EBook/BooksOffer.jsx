80a39d4dd53a71bace0ed4b645333cc465948627
juliangrigera <juliangrigera@gmail.com>
2023-11-23 15:09:19 +0000

    Ebook: Add missing translations

--- src/Pages/EBook/BooksOffer.jsx
+++ src/Pages/EBook/BooksOffer.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import { Modal } from '../../Components/Modal';
 import { MembershipCancelled } from '../../components/EBook/MembershipCancelled';
 import { finishTask } from '../../utils/dark_patterns';
+import { useTranslation } from 'react-i18next';
 
 export const BooksOffer = () => {
+  const { t } = useTranslation();
   const [membershipCancelled, setMembershipCancelled] = useState(false);
 
   const cancelMembership = () => {
@@ -13,49 +15,46 @@ export const BooksOffer = () => {
 
   return (
     <div className="membership offer">
-      <h3 className="text-3xl my-8">Antes de que te vayas...</h3>
+      <h3 className="text-3xl my-8">{t('Ebook.Tease.Before')}</h3>
       <div className="flex justify-center">
         <div className="w-2/5 item">
           <i className="fa-solid fa-sharp fa-calendar-days"></i>
-          <h6>Extender el Período Gratuito</h6>
+          <h6>{t('Ebook.Tease.Extend')}</h6>
           <p className="text-sm text-gray-500">
-            Quedate con nosotros y ampliaremos tu prueba gratuita 30 días
-            adicionales.
+            {t('Ebook.Tease.Stay')}
           </p>
           <button
             type="button"
             className="bg-fuchsia-500 rounded p-2 text-lg text-white hover:bg-fuchsia-600 border border-fuchsia-600"
           >
-            Obtener 30 días más
+            {t('Ebook.Tease.Get30')}
           </button>
         </div>
         <div className="w-2/5 item">
           <i className="fa-solid fa-sharp fa-medal"></i>
-          <h6>Probar Ebookworld Lite por $4.99/Mes</h6>
+          <h6>{t('Ebook.Tease.Try')}</h6>
           <p className="text-sm text-gray-500">
-            Conservarás el acceso a una selección limitada de libros y
-            audiolibros, junto con acceso ilimitado a Instantáneas, documentos,
-            noticias, revistas y partituras.
+            {t('Ebook.Tease.Keep')}
           </p>
           <button
             type="button"
             className="bg-fuchsia-500 rounded p-2 text-lg text-white hover:bg-fuchsia-600 border border-fuchsia-600"
           >
-            Probar Ebookworld Lite
+            {t('Ebook.Tease.TryLite')}
           </button>
         </div>
       </div>
       <p className="cancel">
-        ¿Aún prefieres cancelar?{' '}
+        {t('Ebook.Tease.Still')}{' '}
         <button
           className="underline text-fuchsia-500"
           onClick={cancelMembership}
         >
-          Finalizar cancelación
+          {t('Ebook.Tease.End')}
         </button>
       </p>
       {membershipCancelled && (
-        <Modal title="Membresia cancelada">
+        <Modal title={t('Ebook.Tease.Ended')}>
           <MembershipCancelled />
         </Modal>
       )}
