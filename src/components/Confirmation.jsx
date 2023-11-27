1aae04eafde90abe97a83ae55feeee8775264dee
juancruzgardey <juancruzgardey@hotmail.com>
2023-11-27 10:12:49 -0300

    add finished task modal to BooksOffer page

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -3,11 +3,18 @@ import confirmation from '../assets/CarRental/confirmation.png';
 
 export const Confirmation = () => {
   const { t } = useTranslation();
-  const closeTab = () =>{window.close()};
+  const closeTab = () => {
+    window.close();
+  };
   return (
     <div className="w-full mx-auto text-center">
       <img className="w-48 mx-auto my-4" src={confirmation} />
-      <button class="bg-green-700 hover:bg-green-600 text-white font-bold py-6 px-10 rounded-full text-center text-xl" onClick={closeTab}>{t('Rental.Review.Confirmation.Close')}</button>
+      <button
+        class="bg-green-700 hover:bg-green-600 text-white font-bold py-6 px-10 rounded-full text-center text-xl"
+        onClick={closeTab}
+      >
+        {t('Rental.Review.Confirmation.Close')}
+      </button>
     </div>
   );
 };
