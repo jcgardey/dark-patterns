5ca055f3d2609155ae9b030552e696bbeabe835c
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2023-12-04 11:13:08 -0300

    add labes to Roomio search page

--- src/Pages/Roomio/RoomioSummary.jsx
+++ src/Pages/Roomio/RoomioSummary.jsx
@@ -1,20 +1,20 @@
-import { forwardRef, useState } from 'react';
-import { NavBar } from '../../components/Roomio/NavBar';
-import { Footer } from '../../components/Roomio/Footer';
-import { useForm } from 'react-hook-form';
-import { formatCurrency } from '../../utils/currency';
-import { useTranslation } from 'react-i18next';
-import { finishTask } from '../../utils/dark_patterns';
-import { FinishedTask } from '../../components/FinishedTask';
+import { forwardRef, useState } from "react";
+import { NavBar } from "../../components/Roomio/NavBar";
+import { Footer } from "../../components/Roomio/Footer";
+import { useForm } from "react-hook-form";
+import { formatCurrency } from "../../utils/currency";
+import { useTranslation } from "react-i18next";
+import { finishTask } from "../../utils/dark_patterns";
+import { FinishedTask } from "../../components/FinishedTask";
 
 const Input = forwardRef(
-  ({ id, type = 'text', placeholder, errors, ...props }, ref) => (
+  ({ id, type = "text", placeholder, errors, ...props }, ref) => (
     <input
       id={id}
       ref={ref}
       type={type}
       className={`w-full rounded block p-2${
-        !!errors ? ' border-2 border-red-500' : ''
+        !!errors ? " border-2 border-red-500" : ""
       }`}
       placeholder={placeholder}
       {...props}
@@ -34,26 +34,27 @@ export const RoomioSummary = () => {
     handleSubmit,
     register,
     setValue,
+    getValues,
     formState: { errors },
   } = useForm();
 
   const onSubmit = (data) => {
     setConfirmed(true);
-    finishTask('Roomio');
+    finishTask("Roomio");
   };
 
   const autocompleteCard = () => {
     setShowAutocompleteCard(false);
-    setValue('cc-number', '5490 4234 4899 4324');
-    setValue('cc-name', 'Miriam Flores');
-    setValue('cc-expiry', '12/28');
-    setValue('cc-cvv', '322');
+    setValue("cc-number", "5490 4234 4899 4324");
+    setValue("cc-name", getValues("fullName") || "Miriam Flores");
+    setValue("cc-expiry", "12/28");
+    setValue("cc-cvv", "322");
   };
 
-  const nights = parseInt(localStorage.getItem('hotel-nights')) ?? 0;
+  const nights = parseInt(localStorage.getItem("hotel-nights")) ?? 0;
 
-  const price = (parseFloat(localStorage.getItem('hotel-price')) ?? 0) * nights;
-  const taxes = (parseFloat(localStorage.getItem('hotel-taxes')) ?? 0) * nights;
+  const price = (parseFloat(localStorage.getItem("hotel-price")) ?? 0) * nights;
+  const taxes = (parseFloat(localStorage.getItem("hotel-taxes")) ?? 0) * nights;
 
   const { t } = useTranslation();
 
@@ -67,11 +68,11 @@ export const RoomioSummary = () => {
               <div className="bg-neutral-200 p-6 rounded">
                 <div className="w-1/3">
                   <div className="my-4">
-                    <label htmlFor="fullName">{t('Roomio.Summary.Name')}</label>
+                    <label htmlFor="fullName">{t("Roomio.Summary.Name")}</label>
                     <Input
                       id="fullName"
                       placeholder="Andrea Paz"
-                      {...register('fullName', { required: true })}
+                      {...register("fullName", { required: true })}
                       errors={errors.fullName}
                     />
                   </div>
@@ -81,11 +82,11 @@ export const RoomioSummary = () => {
                     <Input
                       id="email"
                       placeholder="andrea.paz@mail.com"
-                      {...register('email', { required: true })}
+                      {...register("email", { required: true })}
                       errors={errors.email}
                     />
                     {errors.email && (
-                      <FieldError message={'Ingrese un email válido'} />
+                      <FieldError message={"Ingrese un email válido"} />
                     )}
                   </div>
                 </div>
@@ -94,7 +95,7 @@ export const RoomioSummary = () => {
                 <div className="flex my-4">
                   <div className="w-1/3 mr-8 relative">
                     <label htmlFor="cc-number">
-                      {t('Rental.Review.Payment.Card.Number')}
+                      {t("Rental.Review.Payment.Card.Number")}
                     </label>
                     <Input
                       type="text"
@@ -102,8 +103,8 @@ export const RoomioSummary = () => {
                       placeholder="XXXX XXXX XXXX XXXX"
                       onFocus={() => setShowAutocompleteCard(true)}
                       onBlur={() => setShowAutocompleteCard(false)}
-                      {...register('cc-number', { required: true })}
-                      errors={errors['cc-number']}
+                      {...register("cc-number", { required: true })}
+                      errors={errors["cc-number"]}
                     />
                     {showAutocompleteCard && (
                       <div id="autocompleteCard" onClick={autocompleteCard}>
@@ -116,27 +117,27 @@ export const RoomioSummary = () => {
                   </div>
                   <div className="w-1/3">
                     <label htmlFor="email">
-                      {t('Rental.Review.Payment.Card.Holder')}
+                      {t("Rental.Review.Payment.Card.Holder")}
                     </label>
                     <Input
                       type="text"
                       id="cc-name"
                       placeholder="Andrea Paz"
-                      {...register('cc-name', { required: true })}
-                      errors={errors['cc-name']}
+                      {...register("cc-name", { required: true })}
+                      errors={errors["cc-name"]}
                     />
                   </div>
                 </div>
                 <div className="flex my-4">
                   <div className="w-1/6 mr-8">
                     <label htmlFor="cc-expiry">{`${t(
-                      'Rental.Review.Payment.Card.Month'
-                    )}/${t('Rental.Review.Payment.Card.Year')}`}</label>
+                      "Rental.Review.Payment.Card.Month"
+                    )}/${t("Rental.Review.Payment.Card.Year")}`}</label>
                     <Input
                       id="cc-expiry"
                       placeholder="MM/AA"
-                      {...register('cc-expiry', { required: true })}
-                      errors={errors['cc-expiry']}
+                      {...register("cc-expiry", { required: true })}
+                      errors={errors["cc-expiry"]}
                     />
                   </div>
                   <div className="w-1/12">
@@ -145,8 +146,8 @@ export const RoomioSummary = () => {
                       type="password"
                       id="cc-cvv"
                       placeholder="***"
-                      {...register('cc-cvv', { required: true })}
-                      errors={errors['cc-cvv']}
+                      {...register("cc-cvv", { required: true })}
+                      errors={errors["cc-cvv"]}
                     />
                   </div>
                 </div>
@@ -156,12 +157,12 @@ export const RoomioSummary = () => {
             <div className="w-1/4">
               <div className="bg-teal-600 p-4 rounded">
                 <h2 className="text-white font-medium text-3xl">
-                  {t('Roomio.Summary.Summary')}
+                  {t("Roomio.Summary.Summary")}
                 </h2>
                 <div className="py-4 border-b border-white">
                   <div className="my-4">
                     <p className="text-white text-xl font-bold my-1">
-                      {t('Roomio.Summary.Price', { count: nights })}
+                      {t("Roomio.Summary.Price", { count: nights })}
                     </p>
                     <p className="text-right text-xl text-white">
                       {formatCurrency(price)}
@@ -169,7 +170,7 @@ export const RoomioSummary = () => {
                   </div>
                   <div className="my-4">
                     <p className="text-white text-xl font-bold my-1">
-                      {t('Roomio.Summary.Taxes')}
+                      {t("Roomio.Summary.Taxes")}
                     </p>
                     <p className="text-right text-xl text-white">
                       {formatCurrency(taxes)}
@@ -178,7 +179,7 @@ export const RoomioSummary = () => {
                 </div>
                 <div className="flex justify-between py-2">
                   <h4 className="text-white text-3xl ">
-                    {t('Roomio.Summary.Total')}
+                    {t("Roomio.Summary.Total")}
                   </h4>
                   <p className="text-2xl text-white" id="total">
                     {formatCurrency(price + taxes)}
@@ -191,7 +192,7 @@ export const RoomioSummary = () => {
                   className="w-full py-2 bg-blue-600 hover:bg-blue-700 text-white text-2xl rounded"
                   type="submit"
                 >
-                  {t('Roomio.Summary.Buy')}
+                  {t("Roomio.Summary.Buy")}
                 </button>
               </div>
             </div>
--- src/Pages/Roomio/Search.jsx
+++ src/Pages/Roomio/Search.jsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from 'react';
-import './Roomio.css';
-import DatePicker from 'react-datepicker';
+import { useEffect, useState } from "react";
+import "./Roomio.css";
+import DatePicker from "react-datepicker";
 
-import background from '../../assets/Roomio/bg.jpg';
-import { NavBar } from '../../components/Roomio/NavBar';
-import { Footer } from '../../components/Roomio/Footer';
-import { useNavigate } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { updateDarkPatternState } from '../../utils/dark_patterns';
-import { Autocomplete } from '../../components/Form/Autocomplete';
-import cities from '../../utils/cities.json';
+import background from "../../assets/Roomio/bg.jpg";
+import { NavBar } from "../../components/Roomio/NavBar";
+import { Footer } from "../../components/Roomio/Footer";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { updateDarkPatternState } from "../../utils/dark_patterns";
+import { Autocomplete } from "../../components/Form/Autocomplete";
+import cities from "../../utils/cities.json";
 
 export const Search = ({}) => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [adults, setAdults] = useState(1);
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState("");
 
   const navigate = useNavigate();
 
@@ -23,11 +23,11 @@ export const Search = ({}) => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    localStorage.setItem('hotel-start', startDate.toISOString().split('T')[0]);
-    localStorage.setItem('hotel-end', endDate.toISOString().split('T')[0]);
-    localStorage.setItem('hotel-adults', adults);
-    localStorage.setItem('hotel-city', city);
-    navigate('/roomio/results');
+    localStorage.setItem("hotel-start", startDate.toISOString().split("T")[0]);
+    localStorage.setItem("hotel-end", endDate.toISOString().split("T")[0]);
+    localStorage.setItem("hotel-adults", adults);
+    localStorage.setItem("hotel-city", city);
+    navigate("/roomio/results");
   };
 
   useEffect(() => {
@@ -36,69 +36,93 @@ export const Search = ({}) => {
 
   return (
     <>
-    <div style={{ background: `url(${background})`, minHeight: '100vh' }}>
-      <NavBar />
-      <form
-        className="w-11/12 mx-auto flex justify-evenly bg-white/[.3] rounded-lg p-10 mt-32"
-        onSubmit={onSubmit}
-      >
-        <div className="relative w-1/4">
-          <Autocomplete
-            inputClassName="p-2 text-lg rounded w-full"
-            optionClassName="hover:bg-teal-600"
-            placeholder={t('Roomio.Search.Destination')}
-            name="destination"
-            value={city}
-            onChange={(newValue) => setCity(newValue)}
-            suggestedValues={cities.map((c) => `${c.nombre}, ${c.pais}`)}
-          />
-        </div>
-        <div>
-          <DatePicker
-            className="p-2 text-lg rounded mx-2"
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-            selectsStart
-            startDate={startDate}
-            endDate={endDate}
-            minDate={new Date()}
-            dateFormat="dd/MM/yyyy"
-          />
-        </div>
-        <div>
-          <DatePicker
-            className="p-2 text-lg rounded mx-2"
-            selected={endDate}
-            onChange={(date) => setEndDate(date)}
-            selectsEnd
-            startDate={startDate}
-            endDate={endDate}
-            minDate={startDate}
-            dateFormat="dd/MM/yyyy"
-          />
-        </div>
-        <select
-          className="p-2 text-lg rounded mx-2"
-          aria-label="Default select example"
-          value={adults}
-          onChange={(e) => setAdults(e.target.value)}
+      <div style={{ background: `url(${background})`, minHeight: "100vh" }}>
+        <NavBar />
+        <form
+          className="w-11/12 mx-auto flex items-end justify-evenly bg-white/[.3] rounded-lg p-10 mt-32"
+          onSubmit={onSubmit}
         >
-          <option value="">{t('Roomio.Search.People')}</option>
-          <option value="1">{t('Roomio.Search.Adult', { count: 1 })}</option>
-          <option value="2">{t('Roomio.Search.Adult', { count: 2 })}</option>
-          <option value="3">{t('Roomio.Search.Adult', { count: 3 })}</option>
-          <option value="4">{t('Roomio.Search.Adult', { count: 4 })}</option>
-          <option value="5">{t('Roomio.Search.Adult', { count: 5 })}</option>
-        </select>
-        <button
-          type="submit"
-          className="bg-green-700 hover:bg-green-800 p-2 rounded text-white text-lg"
-        >
-          {t('Roomio.Search.Search')}
-        </button>
-      </form>
-    </div>
-    <Footer />
+          <div className="relative w-1/4">
+            <label className="text-white font-bold">
+              {t("Roomio.Search.Destination")}
+            </label>
+            <Autocomplete
+              inputClassName="p-2 text-lg rounded w-full"
+              optionClassName="hover:bg-teal-600"
+              placeholder={t("Roomio.Search.Destination")}
+              name="destination"
+              value={city}
+              onChange={(newValue) => setCity(newValue)}
+              suggestedValues={cities.map((c) => `${c.nombre}, ${c.pais}`)}
+            />
+          </div>
+          <div>
+            <label className="text-white font-bold block">
+              {t("Roomio.Search.Start")}
+            </label>
+            <DatePicker
+              className="p-2 text-lg rounded"
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+              minDate={new Date()}
+              dateFormat="dd/MM/yyyy"
+            />
+          </div>
+          <div>
+            <label className="text-white font-bold block">
+              {t("Roomio.Search.End")}
+            </label>
+            <DatePicker
+              className="p-2 text-lg rounded"
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+              dateFormat="dd/MM/yyyy"
+            />
+          </div>
+          <div>
+            <label className="text-white font-bold block">
+              {t("Roomio.Search.Adults")}
+            </label>
+            <select
+              className="p-2 text-lg rounded h-7"
+              aria-label="Default select example"
+              value={adults}
+              onChange={(e) => setAdults(e.target.value)}
+            >
+              <option value="">{t("Roomio.Search.People")}</option>
+              <option value="1">
+                {t("Roomio.Search.Adult", { count: 1 })}
+              </option>
+              <option value="2">
+                {t("Roomio.Search.Adult", { count: 2 })}
+              </option>
+              <option value="3">
+                {t("Roomio.Search.Adult", { count: 3 })}
+              </option>
+              <option value="4">
+                {t("Roomio.Search.Adult", { count: 4 })}
+              </option>
+              <option value="5">
+                {t("Roomio.Search.Adult", { count: 5 })}
+              </option>
+            </select>
+          </div>
+          <button
+            type="submit"
+            className="bg-green-700 hover:bg-green-800 p-2 rounded text-white text-lg"
+          >
+            {t("Roomio.Search.Search")}
+          </button>
+        </form>
+      </div>
+      <Footer />
     </>
   );
 };
