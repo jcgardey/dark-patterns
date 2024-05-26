edfc515981ef43ecec39cf6831281c92f99a2173
juancruzgardey <juancruzgardey@hotmail.com>
2024-05-26 14:50:55 -0300

    change website group creation based on existing websites

--- src/Pages/Dashboard/CreateWebsiteGroup.jsx
+++ src/Pages/Dashboard/CreateWebsiteGroup.jsx
@@ -1,65 +1,50 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
-import { createWebsiteGroup } from '../../services/dashboard';
+import { createWebsiteGroup, getAllWebsites } from '../../services/dashboard';
 
 export const CreateWebsiteGroup = () => {
   const { register, handleSubmit } = useForm();
   const [websites, setWebsites] = useState([]);
+  const [selectedWebsitesIds, setSelectedWebsitesIds] = useState([]);
 
   const navigate = useNavigate();
 
-  const websitesData = {
-    car_rental: {
-      name: 'Car Rental',
-      url: '/car_rental',
-      instructions: 'Common.TaskCar',
-    },
-    ebook: {
-      name: 'EBook',
-      url: '/ebook',
-      instructions: 'Common.TaskEBook',
-    },
-    roomio: {
-      name: 'Roomio',
-      url: '/roomio',
-      instructions: 'Common.TaskRoomio',
-    },
-    air: {
-      name: 'Air Somewhere',
-      url: '/check_in',
-      instructions: 'Common.TaskCheckin',
-    },
+  useEffect(() => {
+    getAllWebsites().then((data) => setWebsites(data));
+  }, []);
+
+  const handleWebsiteSelection = (event) => {
+    setSelectedWebsitesIds([
+      ...selectedWebsitesIds,
+      parseInt(event.target.value),
+    ]);
   };
 
-  const [newWebsite, setNewWebsite] = useState(null);
-  const [isDark, setIsDark] = useState(false);
-
-  const onNewWebsiteChange = (key) => {
-    const website = websitesData[key];
-    website.url += `?enabled=${isDark}`;
-    setNewWebsite({ ...website, key });
-  };
-
-  const addNewWebsite = () => {
-    setWebsites([...websites, newWebsite]);
-    setNewWebsite(null);
-  };
-
-  const onDeleteWebsite = (name) => {
-    setWebsites(websites.filter((w) => w.name !== name));
+  const onDeleteWebsite = (website) => {
+    setSelectedWebsitesIds(selectedWebsitesIds.filter((w) => w !== website.id));
   };
 
   const onSubmit = (data) => {
-    createWebsiteGroup({ ...data, websites }).then((response) => {
-      navigate('/dashboard');
-    });
+    createWebsiteGroup({ ...data, websites: selectedWebsitesIds }).then(
+      (response) => {
+        navigate('/dashboard');
+      }
+    );
   };
 
   const labelClasses = 'block font-medium';
   const inputClasses = 'border border-gray-500 rounded w-full h-10 p-1';
 
-  console.log(websites);
+  const selectedWebsites = useMemo(
+    () =>
+      selectedWebsitesIds.map((websiteId) =>
+        websites.find((w) => w.id === websiteId)
+      ),
+    [websites, selectedWebsitesIds]
+  );
+
+  console.log('selected websites ', selectedWebsites);
 
   return (
     <div className="my-8 w-1/3 mx-auto">
@@ -101,47 +86,25 @@ export const CreateWebsiteGroup = () => {
               className={inputClasses}
               type="text"
               name="website_name"
-              value={newWebsite?.key ?? ''}
-              onChange={(e) => onNewWebsiteChange(e.target.value)}
+              onChange={handleWebsiteSelection}
             >
               <option value="">Elegir Sitio</option>
-              <option value="air">Air Somewhere</option>
-              <option value="car_rental">Car Rental</option>
-              <option value="ebook">Ebook</option>
-              <option value="roomio">Roomio</option>
+              {websites.map((website) => (
+                <option key={website.id} value={website.id}>
+                  {website.name}
+                </option>
+              ))}
             </select>
-          </div>
-          <div className="my-2">
-            <input
-              type="checkbox"
-              value={isDark}
-              onChange={(e) => setIsDark(e.target.checked)}
-            />{' '}
-            <label>Dark</label>
-          </div>
-          <div className="my-4">
-            <button
-              type="button"
-              className="rounded bg-gray-700 text-white p-2"
-              onClick={addNewWebsite}
-            >
-              Agregar sitio
-            </button>
           </div>
         </div>
         <div className="my-8">
           <h3 className="text-lg font-medium">Sitios agregados</h3>
           <ol>
-            {websites.map((website, i) => (
+            {selectedWebsites.map((website, i) => (
               <li className="flex w-full justify-between my-2" key={i}>
-                <p className="font-light">
-                  {website.name}
-                  {website.url.includes('enabled=true') && (
-                    <span className="font-medium mx-1">Dark</span>
-                  )}
-                </p>
+                <p className="font-light">{website.name}</p>
                 <button
-                  onClick={() => onDeleteWebsite(website.name)}
+                  onClick={() => onDeleteWebsite(website)}
                   className="mx-2 underline text-blue-600"
                 >
                   Eliminar
