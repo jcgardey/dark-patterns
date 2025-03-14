d74724091444473493ed9c4980b2a71bad9e285d
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2025-03-14 19:51:42 +0000

    enable removing follow up group

--- src/Pages/Dashboard/UserSessionsPage.jsx
+++ src/Pages/Dashboard/UserSessionsPage.jsx
@@ -11,9 +11,12 @@ export const UserSessionsPage = () => {
   const [groups, setGroups] = useState([]);
 
   const [assignments, setAssignments] = useState([]);
-  
+  const [loading, setLoading] = useState(false);
+
   const updateAssignment = (user_session_id, follow_up_group_id) => {
-    const newAssignments = assignments.filter(assignment => assignment.user_session_id !== user_session_id);
+    const newAssignments = assignments.filter(
+      (assignment) => assignment.user_session_id !== user_session_id
+    );
     newAssignments.push({ user_session_id, follow_up_group_id });
     setAssignments(newAssignments);
   };
@@ -28,14 +31,24 @@ export const UserSessionsPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    assignFollowUpGroups(assignments)
-  }
+    setLoading(true);
+    assignFollowUpGroups(assignments).then(() => {
+      setLoading(false);
+    });
+  };
 
-  const getAssignmentForSession = (session) => assignments.find(assignment => assignment.user_session_id === session.id)?.follow_up_group_id ?? session.follow_up_group?.id;
+  const getAssignmentForSession = (session) => {
+    const assigned_follow_up = assignments.find(
+      (assignment) => assignment.user_session_id === session.id
+    )?.follow_up_group_id;
+    return assigned_follow_up !== undefined
+      ? assigned_follow_up
+      : session.follow_up_group?.id;
+  };
 
   return (
     <div className="p-8">
-      <h1 className="text-center text-3xl">Usuarios</h1>
+      <h1 className="text-center text-3xl">Usuarios</h1>s
       <div className="flex my-4">
         <p className="w-1/3 font-bold">Email</p>
         <p className="w-1/3 font-bold">Grupo</p>
@@ -48,23 +61,37 @@ export const UserSessionsPage = () => {
             <p className="w-1/3">{session.email}</p>{' '}
             <p className="w-1/3">{session.website_group.name}</p>{' '}
             <select
-              className='border border-gray-500 rounded p-1'
+              className="border border-gray-500 rounded p-1"
               value={getAssignmentForSession(session) ?? ''}
-              onChange={(e) => updateAssignment(session.id, parseInt(e.target.value))}
+              onChange={(e) =>
+                updateAssignment(
+                  session.id,
+                  e.target.value !== '' ? parseInt(e.target.value) : null
+                )
+              }
             >
-              <option value={''}>Seleccionar variante</option>
+              <option value={''}>No asignado</option>
               {groups.map((group) => (
                 <option key={group.id} value={group.id}>
                   {group.name}
                 </option>
               ))}
             </select>
-            <a className="mx-4 underline" href={`/start?sessionId=${session.id}`}>Link</a>
+            <a
+              className="mx-4 underline"
+              href={`/start?sessionId=${session.id}`}
+            >
+              Link
+            </a>
           </div>
         ))}
         <div className="my-4">
-          <button className="rounded bg-sky-700 text-white p-2 disabled:opacity-70">
-                  Guardar</button>
+          <button
+            disabled={loading}
+            className="rounded bg-sky-700 text-white p-2 disabled:opacity-70"
+          >
+            Guardar
+          </button>
         </div>
       </form>
     </div>
