fac51e34f1ad6d3c8dffeeb9e0d82d57d4715e69
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2025-03-14 19:31:08 +0000

    fix sessionId query param in home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Site } from '../components/Home/Site';
 import { useEffect, useState } from 'react';
 
@@ -11,8 +11,9 @@ export const Home = () => {
   const [sites, setSites] = useState([]);
 
   const navigate = useNavigate();
-  const params = useParams();
-  const sessionId = params.sessionId ?? localStorage.getItem('session_id');
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const sessionId = queryParams.get('sessionId') ?? localStorage.getItem('session_id');
 
   useEffect(() => {
     const updateStatus = () => {
